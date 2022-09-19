d459523fc70169c42315f3da0d5fb1e7547ae0a6
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-09-19 18:37:41 +0200

    Add control on unique email for contact of partner

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Serializer\SerializerInterface;
