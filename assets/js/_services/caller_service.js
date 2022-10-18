4d2e528e829127aa1cd25faa369388895fb42792
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-10-18 15:36:50 +0200

    Change url for prof for axios

--- assets/js/_services/caller_service.js
+++ assets/js/_services/caller_service.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { userServices } from '../_services/user_services';
 
+console.log(process.env.APP_ENV);
 const Axios = axios.create({
-    baseURL: process.env.APP_ENV === 'dev' ? 'http://127.0.0.1:8000' : 'https://xtrem-studi.herokuapp.com/'
+    baseURL: process.env.APP_ENV === 'dev' ? 'http://127.0.0.1:8000' : 'https://xtrem-studi.herokuapp.com'
 })
+
+console.dir(Axios)
 
 Axios.interceptors.request.use(request => {
     if(userServices.isConnected()) {
