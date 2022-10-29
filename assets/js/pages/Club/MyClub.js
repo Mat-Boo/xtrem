48abc0ccb61b9055c68a997ca7827c1adaf672b8
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-10-29 15:48:42 +0200

    modify head og myclub page

--- assets/js/pages/Club/MyClub.js
+++ assets/js/pages/Club/MyClub.js
@@ -39,6 +39,7 @@ export default function MyClub() {
             <Helmet>
                 <title>Mon club | Xtrem</title>
                 <meta name="description" content="Xtrem, visualisation du club et ses permissions." />
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
             </Helmet>
             {
                 loader ? 
